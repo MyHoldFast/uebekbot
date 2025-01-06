514a4fba36d54ed4482e33964074409d5435bb06
HoldFast <mbteamru@gmail.com>
2025-01-06 04:36:47 +0300

    update duckduckgo_chat

--- utils/duckduckgo_chat.py
+++ utils/duckduckgo_chat.py
@@ -3,63 +3,41 @@ import json
 
 class DuckDuckGoChat:
     chat_url = "https://duckduckgo.com/duckchat/v1/chat"
-    
-    def __init__(self, vqd=None, model="gpt-4o-mini", proxy=None):
-        self.vqd = vqd or self.fetch_vqd(proxy)
+    common_headers = {
+        "accept": "text/event-stream",
+        "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,ja;q=0.4,de;q=0.3",
+        "cache-control": "no-cache",
+        "dnt": "1",
+        "origin": "https://duckduckgo.com",
+        "pragma": "no-cache",
+        "priority": "u=1, i",
+        "referer": "https://duckduckgo.com/",
+        "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    }
+
+    def __init__(self, model="gpt-4o-mini", proxy=None):
         self.model = model
         self.proxy = proxy
         self.messages = []
-
-    @staticmethod
-    def fetch_vqd(proxy=None):
-        headers = {
-            "x-vqd-accept": "1",
-            "accept": "text/event-stream",
-            "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,ja;q=0.4,de;q=0.3",
-            "cache-control": "no-cache",
-            "dnt": "1",
-            "origin": "https://duckduckgo.com",
-            "pragma": "no-cache",
-            "priority": "u=1, i",
-            "referer": "https://duckduckgo.com/",
-            "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-        }
-        proxies = {"http": proxy, "https": proxy} if proxy else None
-        response = requests.get("https://duckduckgo.com/duckchat/v1/status", headers=headers, proxies=proxies)
-        if response.status_code == 200:
-            return response.headers["x-vqd-4"]
-        raise Exception(f"Failed to initialize chat: {response.status_code}")
+        self.vqd = None
 
     def chat(self, user_input, timeout=30):
+        if not self.vqd:
+            self.vqd = self.fetch_vqd(self.proxy)
+
         self.messages.append({"role": "user", "content": user_input})
         payload = {
             "model": self.model,
             "messages": self.messages,
         }
-        headers = {
-            "accept": "text/event-stream",
-            "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,ja;q=0.4,de;q=0.3",
-            "cache-control": "no-cache",
-            "dnt": "1",
-            "origin": "https://duckduckgo.com",
-            "pragma": "no-cache",
-            "priority": "u=1, i",
-            "referer": "https://duckduckgo.com/",
-            "sec-ch-ua": '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-            "x-vqd-4": self.vqd
-        }
+        headers = DuckDuckGoChat.common_headers.copy()
+        headers["x-vqd-4"] = self.vqd
         proxies = {"http": self.proxy, "https": self.proxy} if self.proxy else None
         response = requests.post(self.chat_url, headers=headers, json=payload, stream=True, proxies=proxies, timeout=timeout)
         if response.status_code != 200:
@@ -78,3 +56,13 @@ class DuckDuckGoChat:
         result = "".join(results)
         self.messages.append({"role": "assistant", "content": result})
         return result
+
+    @staticmethod
+    def fetch_vqd(proxy=None):
+        headers = DuckDuckGoChat.common_headers.copy()
+        headers["x-vqd-accept"] = "1"
+        proxies = {"http": proxy, "https": proxy} if proxy else None
+        response = requests.get("https://duckduckgo.com/duckchat/v1/status", headers=headers, proxies=proxies)
+        if response.status_code == 200:
+            return response.headers["x-vqd-4"]
+        raise Exception(f"Failed to initialize chat: {response.status_code}")
