a4bd07dfb7aed5e264cb8b343bc81fc8f49f5a7f
HoldFast <mbteamru@gmail.com>
2025-02-03 16:04:46 +0300

    fix qwen

--- handlers/qwen.py
+++ handlers/qwen.py
@@ -50,11 +50,12 @@ def split_message(text, limit=4000):
 @router.message(Command("qwen", ignore_case=True))
 async def cmd_qwen(message: Message, command: CommandObject, bot: Bot):
     if message.reply_to_message:
-        user_input = message.reply_to_message.text
+        user_input = message.reply_to_message.text or ""
         if command.args:
             user_input += '\n' + command.args
     else:
-        user_input = command.args if command.args else ''
+        user_input = command.args if command.args else ""
+
  
     user_language = message.from_user.language_code or DEFAULT_LANGUAGE
     _ = get_localization(user_language)
