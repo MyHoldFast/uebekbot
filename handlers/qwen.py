9f05acacf18f481989bf3b6ee9999acda9640d7b
HoldFast <mbteamru@gmail.com>
2025-03-07 01:14:35 +0300

    fix qwen

--- handlers/qwen.py
+++ handlers/qwen.py
@@ -1,4 +1,3 @@
-
 from aiogram import Router, Bot, html
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
@@ -12,22 +11,23 @@ from utils.command_states import check_command_enabled
 from pylatexenc.latex2text import LatexNodes2Text
 import re
 
-context_db, ContextQuery = DB('db/qwen_context.json').get_db()
+context_db, ContextQuery = DB("db/qwen_context.json").get_db()
 router = Router()
 
-qwen_accs_str = os.getenv('QWEN_ACCS')
+qwen_accs_str = os.getenv("QWEN_ACCS")
 qwen_accs = json.loads(qwen_accs_str)
 
 acc_index = 0
 last_update_time = None
 
-MESSAGE_EXPIRY = 3 * 60 * 60 
+MESSAGE_EXPIRY = 3 * 60 * 60
+
 
 def load_messages(user_id):
     context_item = context_db.get(ContextQuery().uid == user_id)
     if context_item:
-        messages = json.loads(context_item.get('messages', '[]'))
-        timestamp = float(context_item.get('timestamp', 0))
+        messages = json.loads(context_item.get("messages", "[]"))
+        timestamp = float(context_item.get("timestamp", 0))
         if time.time() - timestamp < MESSAGE_EXPIRY:
             return messages
     return []
@@ -36,17 +36,18 @@ def load_messages(user_id):
 def save_messages(user_id, messages):
     getcontext = ContextQuery().uid == user_id
     context_item = context_db.get(getcontext)
-    
+
     new_data = {
-        'uid': user_id,
-        'messages': json.dumps(messages, ensure_ascii=False),
-        'timestamp': time.time()
+        "uid": user_id,
+        "messages": json.dumps(messages, ensure_ascii=False),
+        "timestamp": time.time(),
     }
 
     if context_item:
         context_db.update(new_data, getcontext)
     else:
-        context_db.insert(new_data) 
+        context_db.insert(new_data)
+
 
 def remove_messages(user_id):
     getcontext = ContextQuery()
@@ -54,81 +55,112 @@ def remove_messages(user_id):
 
 
 def split_message(text, limit=4000):
-    return [text[i:i+limit] for i in range(0, len(text), limit)]
+    return [text[i : i + limit] for i in range(0, len(text), limit)]
+
 
 def process_latex(text):
     return re.sub(
-        r'(?m)^\s*(\$\$|\\\[)\s*\n(.*?)\n\s*(\$\$|\\\])\s*$',
+        r"(?m)^\s*(\$\$|\\\[)\s*\n(.*?)\n\s*(\$\$|\\\])\s*$",
         lambda match: LatexNodes2Text().latex_to_text(match.group(2)),
         text,
-        flags=re.DOTALL
+        flags=re.DOTALL,
     )
 
-headers = {
-    'authorization': 'Bearer '+qwen_accs[0]['bearer'],      
-    'content-type': 'application/json',    
-    'bx-v': '2.5.0',
-    'cache-control': 'no-cache',
-    'content-type': 'application/json',
-    #'cookie': 'YOUR_COOKIES_HERE',
-    'dnt': '1',
-    'origin': 'https://chat.qwenlm.ai',
-    'pragma': 'no-cache',
-    'priority': 'u=1, i',
-    'referer': 'https://chat.qwenlm.ai/c/6577ef55-cf1c-4136-bb96-0fc0b8603c51',
-    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
-    'x-request-id': qwen_accs[0]['x']
+
+cookies = {
+    "x-ap": "eu-central-1",
+    "ssxmod_itna": "YqIxBGqmqxnD2D4MEDcWx0DUOqeq79DGHDyxWF50CDmxjKidqDUQyj3/Ax6AHrEZD5eCDBMrPrDmqi1XDYPGfeZShxQBD7q5sWQtiw+G5oGpAxj=QFhj8Qe1Mzjxu7r+e0aDbqGkeKZDdrDYYDC4GwDGoD34DiDDPDb8NDAueD7qDF7WR36nrvDYPKh4DmDGYK=QEYDDtDidNDKdp52DDl0YHQ8DdxoiWTudoT+1b4QZh57eDMbxGXf0OQ7mcvwKq==ZBmxpYqxB6XxBQd=OuZ+aI5glbiPOGnGHmLK0Drf0Y4fbhK0GjKz5KB4xlxF4bx8GKK0q0G4QDx0hY4WiU4DG457iQHgxDe4x4CwLCvd1+0zQmY8B=fl3/gx5A4qAz5nmSBxlA4hExOBz3ixArh0Cz4D",
+    "ssxmod_itna2": "YqIxBGqmqxnD2D4MEDcWx0DUOqeq79DGHDyxWF50CDmxjKidqDUQyj3/Ax6AHrEZD5erDGbWPD9iim+K=DjRwKazfDqb=3D05bqNsZG3xQ8olZ2u3enb10T4NnSK3h62zbN/dAgc/RQs9hKKNEmFe/HWc1BOraGsWoKOGhhOFlKbqnGsIrYbSrpHwgyqzoSDmKSkA8D8U=GKHiSOYEiWa7GKQ49kkIYsAUvH=7KxAt0DYbGONGWP8GmsiT7ErPkPAInHKGnk2gP7IEhUXAr8XrKcNO08UOKbmg91=jP8PTKa9X9qqUfF=BP7ZK1ZDQiAxn=wEPUZeyrKwPpn7DKo3ZnEfA5Aj5VhAgA5HZe=jK0n65BiVDPn0Xr96=fGAxKfeTX6duEier5WhYkUKeoIz/PylQ6GIHYogCKZ2YfOPKpi190yWxth58z5hKjp1UrrEv/Ir+LkOc/px++xaStW==PPRBmqggDLSpebUj76XSvNZe4E0VnRTz5MPj/6jOhIKDu4FaBB5eKbgKcqQRf2Ny0NEn7ecj2tG+bX23RXQIy75vlPkto=I0r8t0i5ROIMnHgonog707Ns1kTtibOd3OaO8dU3Xi6p1+U1rvwyYvjdaSwvXUoFWu7Gm0IK=3=RQBrfb37bxxl0yxLhgD7pYcy7DrnpxWxC0yK7t7rpyNa/cLSLwKIs/fKZ4W7D7iKjoS5xeGtDWe13IuiYHDzQt04rhKN78YKVvwDir2vZDdqchFCAWQCmh+=tzxnCdKDfAsA7Yc=O+YGxgb6e5k0hGirT7AdPYKmQTcOaNFDrcO5+xjovc/GDa7SmqeW6YeuBWtD6Htjq3ijADbrKQp70rDI7GDD",
 }
+
+headers = {
+    "accept": "*/*",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6,zh;q=0.5,ja;q=0.4,de;q=0.3",
+    "authorization": "Bearer " + qwen_accs[0]["bearer"],
+    "bx-v": "2.5.28",
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+    "dnt": "1",
+    "origin": "https://chat.qwen.ai",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "referer": "https://chat.qwen.ai/",
+    "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+    "x-request-id": qwen_accs[0]["x"],
+}
+
 
 @router.message(Command("qwen", ignore_case=True))
 @check_command_enabled("qwen")
 async def cmd_qwen(message: Message, command: CommandObject, bot: Bot):
     if message.reply_to_message:
-        user_input = message.reply_to_message.text or message.reply_to_message.caption or ""
+        user_input = (
+            message.reply_to_message.text or message.reply_to_message.caption or ""
+        )
         if command.args:
-            user_input += '\n' + command.args
+            user_input += "\n" + command.args
     else:
         user_input = command.args if command.args else ""
-    
 
     user_language = message.from_user.language_code or DEFAULT_LANGUAGE
     _ = get_localization(user_language)
-    
+
     if not user_input:
         await message.reply(_("qwen_help"))
         return
 
-    user_id = message.from_user.id    
+    user_id = message.from_user.id
     messages = load_messages(user_id)
-    
-    messages.append({"role": "user", "content": user_input})
-    await bot.send_chat_action(chat_id=message.chat.id, action='typing')
-    
-    url = 'https://chat.qwenlm.ai/api/chat/completions'    
-    data = {"stream": False, "chat_type": "t2t", "model": "qwen-max-latest", "messages": messages}
-    
-    async with aiohttp.ClientSession() as session:
+
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input,
+            "chat_type": "t2t",
+            "extra": {},
+            "feature_config": {"thinking_enabled": False},
+        }
+    )
+    await bot.send_chat_action(chat_id=message.chat.id, action="typing")
+
+    url = "https://chat.qwen.ai/api/chat/completions"
+    data = {
+        "stream": False,
+        "incremental_output": False,
+        "chat_type": "t2t",
+        "model": "qwen-max-latest",
+        "messages": messages,
+        "session_id": str(uuid.uuid4()),
+        "chat_id": str(uuid.uuid4()),
+        "id": str(uuid.uuid4()),
+    }
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
         try:
             async with session.post(url, headers=headers, json=data, timeout=120) as r:
                 if r.status == 200:
                     result = await r.json()
-                    assistant_reply = result.get("choices", [{}])[0].get("message", {}).get("content", "Ошибка")
-                    
+                    assistant_reply = (
+                        result.get("choices", [{}])[0]
+                        .get("message", {})
+                        .get("content", "Ошибка")
+                    )
+
                     formatted_reply = process_latex(telegram_format(assistant_reply))
                     chunks = split_message(formatted_reply)
-                    
+
                     for chunk in chunks:
                         try:
                             await message.reply(chunk, parse_mode="HTML")
                         except Exception:
                             await message.reply(html.quote(chunk))
-                    
+
                     messages.append({"role": "assistant", "content": assistant_reply})
                     save_messages(user_id, messages)
                 else:
@@ -154,63 +186,80 @@ async def cmd_qwenrm(message: Message, bot: Bot):
 @check_command_enabled("qwenimg")
 async def cmd_qwenimg(message: Message, command: CommandObject, bot: Bot):
     if message.reply_to_message:
-        user_input = message.reply_to_message.text or message.reply_to_message.caption or ""
+        user_input = (
+            message.reply_to_message.text or message.reply_to_message.caption or ""
+        )
         if command.args:
-            user_input += '\n' + command.args
+            user_input += "\n" + command.args
     else:
         user_input = command.args if command.args else ""
 
     user_language = message.from_user.language_code or DEFAULT_LANGUAGE
     _ = get_localization(user_language)
-    
+
     if not user_input:
         await message.reply(_("qwenimghelp"))
         return
-    
-    url = 'https://chat.qwenlm.ai/api/chat/completions'
+
+    url = "https://chat.qwen.ai/api/chat/completions"
     data = {
-        "stream": False, "chat_type": "t2i", "model": "qwen-plus-latest", 
-        "size": "1280*720", "messages": [{"role": "user", "content": user_input}],
-        "id": str(uuid.uuid4()), "chat_id": str(uuid.uuid4())
-    }    
-    
+        "stream": False,
+        "chat_type": "t2i",
+        "model": "qwen-plus-latest",
+        "size": "1280*720",
+        "messages": [{"role": "user", "content": user_input}],
+        "id": str(uuid.uuid4()),
+        "chat_id": str(uuid.uuid4()),
+    }
+
     async def make_request(session, url, headers, data, message, sent_message):
-        global acc_index, last_update_time    
-        
-        headers['Authorization'] = 'Bearer ' + qwen_accs[acc_index]['bearer']
-        headers['x-request-id'] = qwen_accs[acc_index]['x']    
+        global acc_index, last_update_time
+
+        headers["Authorization"] = "Bearer " + qwen_accs[acc_index]["bearer"]
+        headers["x-request-id"] = qwen_accs[acc_index]["x"]
 
         async with session.post(url, headers=headers, json=data, timeout=120) as r:
-            if r.status == 200:
-                result = await r.json()
-                task_id = result['messages'][1]['extra']['wanx']['task_id']
-                await check_task_status(session, task_id, message, sent_message)
-            elif r.status == 429:                
-                if last_update_time is not None:
-                    time_diff = time.time() - last_update_time
-                    if time_diff > 86400: 
-                        acc_index = 0 
+            try:
+                if r.status == 200:
+                    result = await r.json()
+                    task_id = result["messages"][1]["extra"]["wanx"]["task_id"]
+                    await check_task_status(session, task_id, message, sent_message)
+                elif r.status == 429:
+                    if last_update_time is not None:
+                        time_diff = time.time() - last_update_time
+                        if time_diff > 86400:
+                            acc_index = 0
 
-                if acc_index + 1 < len(qwen_accs):
-                    acc_index += 1
-                    last_update_time = time.time()
-                    await session.close()
-                    async with aiohttp.ClientSession() as new_session:
-                        try:
-                            await make_request(new_session, url, headers, data, message, sent_message)
-                        except Exception:
-                            await safe_delete(sent_message)
-                            await message.reply(_("qwenimg_err"))
+                    if acc_index + 1 < len(qwen_accs):
+                        acc_index += 1
+                        last_update_time = time.time()
+                        await session.close()
+                        async with aiohttp.ClientSession() as new_session:
+                            try:
+                                await make_request(
+                                    new_session,
+                                    url,
+                                    headers,
+                                    data,
+                                    message,
+                                    sent_message,
+                                )
+                            except Exception:
+                                await safe_delete(sent_message)
+                                await message.reply(_("qwenimg_err"))
+                    else:
+                        await safe_delete(sent_message)
+                        await message.reply(_("qwenimg_err"))
                 else:
                     await safe_delete(sent_message)
                     await message.reply(_("qwenimg_err"))
-            else:
+            except Exception:
                 await safe_delete(sent_message)
                 await message.reply(_("qwenimg_err"))
-    
-    sent_message = await message.reply(_("qwenimg_gen"))    
-    
-    async with aiohttp.ClientSession() as session:
+
+    sent_message = await message.reply(_("qwenimg_gen"))
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
         await make_request(session, url, headers, data, message, sent_message)
 
 
@@ -220,27 +269,33 @@ async def safe_delete(message):
     except TelegramBadRequest:
         pass
 
+
 async def check_task_status(session, task_id, message, sent_message):
     user_language = message.from_user.language_code or DEFAULT_LANGUAGE
     _ = get_localization(user_language)
-    status_url = f'https://chat.qwenlm.ai/api/v1/tasks/status/{task_id}'
+    status_url = f"https://chat.qwen.ai/api/v1/tasks/status/{task_id}"
     while True:
         async with session.get(status_url, headers=headers, timeout=180) as r:
-            if r.status == 200:
-                result = await r.json()
-                task_status = result.get("task_status", "")
-                if task_status == "success":
-                    image_url = result.get("content", "")
-                    await safe_delete(sent_message)
-                    await message.reply_photo(photo=image_url)
-                    break
-                elif task_status in ["failed", "error"]:
+            try:
+                if r.status == 200:
+                    result = await r.json()
+                    task_status = result.get("task_status", "")
+                    if task_status == "success":
+                        image_url = result.get("content", "")
+                        await safe_delete(sent_message)
+                        await message.reply_photo(photo=image_url)
+                        break
+                    elif task_status in ["failed", "error"]:
+                        await safe_delete(sent_message)
+                        await message.reply(_("qwenimg_err"))
+                        break
+                    else:
+                        await asyncio.sleep(5)
+                else:
                     await safe_delete(sent_message)
                     await message.reply(_("qwenimg_err"))
                     break
-                else:
-                    await asyncio.sleep(5)
-            else:
+            except Exception:
                 await safe_delete(sent_message)
                 await message.reply(_("qwenimg_err"))
                 break
