71e08b3c466fa1f3fff5ce7061d77bb016f83d0a
HoldFast <mbteamru@gmail.com>
2025-03-27 02:31:27 +0300

    set qwen as default

--- handlers/pm.py
+++ handlers/pm.py
@@ -6,7 +6,7 @@ from localization import get_localization, DEFAULT_LANGUAGE
 from handlers.gpt import process_gpt, process_gemini
 from handlers.qwen import cmd_qwen
 
-router = Router() 
+router = Router()
 
 @router.message(Command("start"), F.chat.type == ChatType.PRIVATE)
 async def cmd_start(message: Message):
@@ -16,7 +16,13 @@ async def cmd_start(message: Message):
 
     await message.answer(start_text, parse_mode="Markdown")
 
-@router.message(F.chat.type == ChatType.PRIVATE, lambda message: message.text and not message.text.startswith("/"), F.content_type == ContentType.TEXT)
+@router.message(
+    F.chat.type == ChatType.PRIVATE,
+    lambda message: message.text and not message.text.startswith("/"),
+    F.content_type == ContentType.TEXT,
+    F.forward_from == None,
+    F.forward_from_chat == None 
+)
 async def pm(message: Message, bot: Bot):
     command = CommandObject(command=None, args=message.text)
     await cmd_qwen(message, command, bot)
@@ -24,19 +30,21 @@ async def pm(message: Message, bot: Bot):
 
 @router.message(
     F.chat.type == ChatType.PRIVATE,
-    F.content_type == ContentType.PHOTO
+    F.content_type == ContentType.PHOTO,
+    F.forward_from == None,  
+    F.forward_from_chat == None 
 )
 async def handle_photo(message: Message, bot: Bot):
-    #user_language = message.from_user.language_code or DEFAULT_LANGUAGE
-    command = CommandObject(command=None, args=message.text)
-    user_id = message.from_user.id
+    command = CommandObject(command=None, args=message.caption) 
     photo = message.reply_to_message.photo[-1] if message.reply_to_message and message.reply_to_message.photo else None
     photo = photo or (message.photo[-1] if message.photo else None)
     await process_gemini(message, command, bot, photo)
 
 @router.message(
     F.chat.type == ChatType.PRIVATE,
-    F.content_type.not_in({ContentType.TEXT, ContentType.PHOTO})
+    F.content_type.not_in({ContentType.TEXT, ContentType.PHOTO}),
+    F.forward_from == None,
+    F.forward_from_chat == None
 )
 async def handle_other_content(message: Message, bot: Bot):
     user_language = message.from_user.language_code or DEFAULT_LANGUAGE
